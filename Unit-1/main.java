b85185f2c90944ce4ecf28e3e9adfc801bbee3c3
AllusiveWheat <auraud.zarafshar@gmail.com>
2021-08-18 18:51:20 -0700

    I was working on math hw lol

--- Unit-1/main.java
+++ Unit-1/main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        double x = 5.88/13.669;;
+        //Math.acos(x);
+        
+        Math.cos(x);
+        System.out.println(x);
     }
 }
---
+++ Unit-1/math.c
@@ -0,0 +1,8 @@
+#include <math.h>
+
+int main ()
+{
+    double x = 
+   float y = acos(x);
+    printf(y);
+}
