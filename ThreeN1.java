4c8a90e76dd52360313b4b9682b5650d63eca8e9
Allusive <allusivewheat@gmail.com>
2021-09-18 19:25:46 -0700

    Finished ThreeN1

--- ThreeN1.java
+++ ThreeN1.java
@@ -1,27 +1,33 @@
 
 public class ThreeN1 {
     public static void main(String[] args) {
+        // This took way to long to figure out.
+        // Timestamp 1632018003
         System.out.println("Enter A Postive Integer");
-        // check if n is a positive integer
-
-        int n = TextIO.getInt();
-        if (n < 0) {
-            System.out.println("Invalid Input");
-            return;
-        } else {
-            int x = 0;
-            while (n != 1) {
-                if (n % 2 == 0) {
-                    n = n / 2;
-                } else {
-                    n = 3 * n + 1;
-                }
-                // System.out.println(n);
-                x++;
-            }
-            System.out.println(x + " steps");
-
+        int input = TextIO.getInt();
+        int max = 0;
+        if (input < 0) {
+            System.out.println("Please enter a positive integer");
         }
+        int maxNum = 0;
+        for (int i = 1; i <= input; i++) {
+            int count = 0;
+            int num = i;
+            while (num != 1) {
+                if (num % 2 == 0) {
+                    num = num / 2;
+                } else {
+                    num = 3 * num + 1;
+                }
+                count++;
+            }
+            if (count > max) {
+                max = count;
+                maxNum = i;
+            }
+        }
+        System.out
+                .println("The number with the most steps is " + maxNum + " with " + max + " steps");
     }
-
 }
+
