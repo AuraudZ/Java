2c1acb95f131f2f22be1c2ee33304953b298ce23
Allusive <allusivewheat@gmail.com>
2021-09-18 21:47:33 -0700

    Finished

--- ThreeN1.java
+++ ThreeN1.java
@@ -1,16 +1,19 @@
-
+// Auraud 9/18/2021
+// Mr Smith I spent a long time trying to figure out how to do this. But I wrote
+// a another function but it is annoying how java doesn't have multi returns so
+// I had to do some array stuff but anyway I got it working. And I hope you like
+// what I did :)
+// There a lot of differnt way to do this I know I kinda of went above and
+// beyond to pull this off but well
 public class ThreeN1 {
     public static void main(String[] args) {
         // This took way to long to figure out.
         // Timestamp 1632018003
         // Inatlize Vars
-        // Mr Smith I spent a long time trying to figure out how to do this. But I wrote
-        // a another function but it is annoying how java doesn't have multi returns so
-        // I had to do some array stuff but anyway I got it working. And I hope you like
-        // what I did :)
         int input = TextIO.getInt();
         int count = 0;
         int max = 0;
+        // Logic
         System.out.println("Enter A Postive Integer");
         if (input < 0) {
             System.out.println("Please enter a positive integer");
