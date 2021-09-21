11718b9706a391955476ba03b7ac1c99eb27b26d
Allusive <allusivewheat@gmail.com>
2021-09-20 17:19:01 -0700

    Finished Divisor Counter

--- Unit-3/DivisorCounter.java
+++ Unit-3/DivisorCounter.java
@@ -1,22 +1,31 @@
 public class DivisorCounter {
+
     public static void main(String[] args) {
-        System.out.println("Please Enter a Postive Intger ");
-        // 2) Figures out which integer(s) between 1 and the inputted number has the
-        // greatest amount of divisors
-
-        int divisor = TextIO.getlnInt();
-        int count = 0;
-
-        for (int i = 1; i <= divisor; i++) {
-            if (divisor % i == 0) {
-                count++;
+        System.out.println("Please Enter A Postive Integer");
+        int input = TextIO.getlnInt();
+        int[] nums = new int[input];
+        int[] divisors = new int[input];
+        int maxDivisors = 0;
+        for (int i = 0; i < input; i++) {
+            nums[i] = i + 1;
+            divisors[i] = 0;
+            for (int j = 1; j <= nums[i]; j++) {
+                if (nums[i] % j == 0) {
+                    divisors[i]++;
+                }
+            }
+            if (divisors[i] > maxDivisors) {
+                maxDivisors = divisors[i];
+                int max = nums[i];
+                int maxIndex = i;
             }
         }
-        System.out.println("The Amount of divisors is " + count);
-    }
-
-    public int[] DivisorCounter(int number) {
-
-        return new int[number];
+        System.out.println("The number(s) between 1 and " + input + " with the most divisors are: ");
+        for (int i = 0; i < input; i++) {
+            if (divisors[i] == maxDivisors) {
+                System.out.println(nums[i] + " ");
+            }
+        }
+        System.out.println("They each have " + maxDivisors + " divisors.");
     }
 }
