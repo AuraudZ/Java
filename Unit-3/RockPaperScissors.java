d2ce16ebd0fecda4217adb0979ed25b0b16773a0
Allusive <allusivewheat@gmail.com>
2021-09-22 15:52:57 -0700

    Added ASCII Art  for each choice

--- Unit-3/RockPaperScissors.java
+++ Unit-3/RockPaperScissors.java
@@ -9,6 +9,15 @@ public class RockPaperScissors {
     }
 
     public static void Run(String input, int bestOf) {
+        String youRockCompRock = "You         Comp\n\n __           __ \n/  \\         /  \\\n\\__/         \\__/";
+        String youRockCompPaper = "You         Comp\n\n __            ___\n/  \\          /  /\n\\__/         /__/";
+        String youRockCompScissors = "You         Comp\n\n __          O O\n/  \\          X\n\\__/         / \\";
+        String youPaperCompRock = "You         Comp\n\n  ___          __ \n /  /         /  \\\n/__/          \\__/";
+        String youPaperCompPaper = "You         Comp\n\n  ___           ___\n /  /          /  /\n/__/          /__/";
+        String youPaperCompScissors = "You         Comp\n\n  ___         O O\n /  /          X\n/__/          / \\";
+        String youScissorsCompRock = "You         Comp\n\nO O          __ \n X          /  \\\n/ \\         \\__/";
+        String youScissorsCompPaper = "You         Comp\n\nO O           ___\n X           /  /\n/ \\         /__/";
+        String youScissorsCompScissors = "You         Comp\n\nO O         O O\n X           X\n/ \\         / \\";
         String compChoiceString = "";
         int compChoice = (int) Math.random() * 3;
         switch (compChoice) {
@@ -31,34 +40,40 @@ public class RockPaperScissors {
             if (input.equals("Rock")) {
                 if (compChoiceString.equals("Rock")) {
                     tie = true;
-                    System.out.println("Tie!");
+                    System.out.println(youRockCompRock);
                 } else if (compChoiceString.equals("Paper")) {
                     compWin++;
-                    System.out.println("You lose!");
+                    System.out.println(youRockCompPaper);
                 } else {
                     playerWin++;
-                    System.out.println("You win!");
+                    System.out.println(youRockCompScissors);
                 }
             } else if (input.equals("Paper")) {
                 if (compChoiceString.equals("Rock")) {
                     playerWin++;
                     System.out.println("You win!");
+                    System.out.println(youPaperCompRock);
                 } else if (compChoiceString.equals("Paper")) {
+                    System.out.println(youPaperCompPaper);
                     tie = true;
                     System.out.println("Tie!");
                 } else {
+                    System.out.println(youPaperCompScissors);
                     compWin++;
                     System.out.println("You lose!");
 
                 }
             } else if (input.equals("Scissors")) {
                 if (compChoiceString.equals("Rock")) {
+                    System.out.println(youScissorsCompRock);
                     compWin++;
                     System.out.println("You lose!");
                 } else if (compChoiceString.equals("Paper")) {
+                    System.out.println(youScissorsCompPaper);
                     playerWin++;
                     System.out.println("You win!");
                 } else {
+                    System.out.println(youScissorsCompScissors);
                     tie = true;
                     System.out.println("Tie!");
                 }
