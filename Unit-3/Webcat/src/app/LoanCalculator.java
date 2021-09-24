eb50f080714877a07e17eb7bb640174d3d256aee
Allusive <auraud.zarafshar@gmail.com>
2021-09-24 14:45:36 -0700

    More Webcat

--- Unit-3/Webcat/src/app/LoanCalculator.java
+++ Unit-3/Webcat/src/app/LoanCalculator.java
@@ -14,11 +14,6 @@ public class LoanCalculator {
         double principalPaid = 0;
         int months = 12;
         StringBuilder sb = new StringBuilder();
-        // make sure the first line is lined up
-
-        sb.append("Month").append("\t").append("Interest").append("\t").append("Principal").append("\t")
-                .append("Balance").append("\n");
-
         if (monthlyPayment < 0) {
             return "Monthly payment is negative";
         }
@@ -27,30 +22,19 @@ public class LoanCalculator {
             return "Monthly payment is greater than the load amount";
         }
 
-        for (int i = 1; i <= months; i++) {
-            double interest = balance * monthlyInterestRate;
-            double principal = monthlyPayment - interest;
-            balance = balance - principal;
-            interestPaid += interest;
-            principalPaid += principal;
-            sb.append(String.format("%d\t\t%.2f\t\t%.2f\t\t%.2f\n", i, interestPaid, principalPaid, balance));
+        sb.append(String.format("Month     Interest        Payment         Remaining " + "\n"));
+        sb.append(String.format("%1$-10s%2$-15s%3$-15s%4$-15s", "", "", "", "")).append("\n");
+        for (int i = 0; i < months; i++) {
+            double monthlyInterest = balance * monthlyInterestRate;
+            double monthlyPrincipal = monthlyPayment - monthlyInterest;
+            balance -= monthlyPrincipal;
+            interestPaid += monthlyInterest;
+            principalPaid += monthlyPrincipal;
+            sb.append(String.format("%1$-10d%2$-15.2f%3$-15.2f%4$-15.2f", i + 1, monthlyInterest, monthlyPrincipal,
+                    balance)).append("\n");
         }
-
-        sb.append("\nTotal paid: " + (interestPaid + principalPaid));
-        sb.append("\nTotal interest paid: " + interestPaid);
-        sb.append("\nTotal principal paid: " + principalPaid);
-        sb.append("\nTotal balance: " + balance);
-        amoTable = sb.toString();
-        return amoTable;
+        sb.append(String.format("%1$-10s%2$-15.2f%3$-15.2f%4$-15.2f", "Total", interestPaid, principalPaid, balance))
+                .append("\n");
+        return sb.toString();
     }
 }
-// for (int i = 1; i <= months; i++) {
-// interestPaid = monthlyInterestRate * balance;
-// principalPaid = monthlyPayment - interestPaid;
-// balance = balance - principalPaid;
-// sb.append(i + "\t\t" + interestPaid + "\t\t" + principalPaid + "\t\t" +
-// balance + "\n");
-// }
-// amoTable = sb.toString();
-// return amoTable;
-// }}
