74920b644b3d1ec48894406011b4d11dc10b4a73
Allusive <auraud.zarafshar@gmail.com>
2021-09-26 22:10:44 -0700

    more webcat

--- Unit-3/Webcat/src/app/LoanCalculator.java
+++ Unit-3/Webcat/src/app/LoanCalculator.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.text.NumberFormat;
+
 public class LoanCalculator {
     public static void main(String[] args) {
         String table = createAmortizationTable(10000, 950, 0.17);
@@ -8,9 +10,8 @@ public class LoanCalculator {
 
     public static String createAmortizationTable(double loadAmount, double monthlyPayment, double annualInterestRate) {
         double monthlyInterestRate = annualInterestRate / 12;
-        String amoTable = "";
-        double balance = loadAmount;
-        double interestPaid = 0;
+        double payment = loadAmount;
+        double monthlyInterestPayment = 0;
         double principalPaid = 0;
         int months = 12;
         StringBuilder sb = new StringBuilder();
@@ -21,20 +22,28 @@ public class LoanCalculator {
         if (monthlyPayment > loadAmount) {
             return "Monthly payment is greater than the load amount";
         }
+        double paymentCheck = 0;
 
-        sb.append(String.format("Month     Interest        Payment         Remaining " + "\n"));
-        sb.append(String.format("%1$-10s%2$-15s%3$-15s%4$-15s", "", "", "", "")).append("\n");
-        for (int i = 0; i < months; i++) {
-            double monthlyInterest = balance * monthlyInterestRate;
-            double monthlyPrincipal = monthlyPayment - monthlyInterest;
-            balance -= monthlyPrincipal;
-            interestPaid += monthlyInterest;
-            principalPaid += monthlyPrincipal;
-            sb.append(String.format("%1$-10d%2$-15.2f%3$-15.2f%4$-15.2f", i + 1, monthlyInterest, monthlyPrincipal,
-                    balance)).append("\n");
+        sb.append(String.format("Month     Interest        Payment         Remaining%n"));
+        for (int i = 1; i < months; i++) {
+            // payment is static untill final month
+
+            // principalPaid is the monthly payment
+            if (principalPaid < 0) {
+                return "Monthly payment is greater than the load amount";
+            }
+
+            // calculate interest
+            monthlyInterestPayment = payment * monthlyInterestRate;
+            // calculate principal paid
+            principalPaid = monthlyPayment - monthlyInterestPayment;
+            // calculate remaining balance
+
+            // format and print
+            sb.append(
+                    String.format("%2d %15.2f %15.2f %15.2f%n", i + 1, monthlyInterestPayment, paymentCheck, payment));
+
         }
-        sb.append(String.format("%1$-10s%2$-15.2f%3$-15.2f%4$-15.2f", "Total", interestPaid, principalPaid, balance))
-                .append("\n");
         return sb.toString();
     }
 }
