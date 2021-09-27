684d8ab4fb64c9c6542987849485fc91e7bebef3
Allusive <auraud.zarafshar@gmail.com>
2021-09-27 12:14:25 -0700

    webcat

--- Unit-3/Webcat/src/app/LoanCalculator.java
+++ Unit-3/Webcat/src/app/LoanCalculator.java
@@ -10,39 +10,27 @@ public class LoanCalculator {
 
     public static String createAmortizationTable(double loadAmount, double monthlyPayment, double annualInterestRate) {
         double monthlyInterestRate = annualInterestRate / 12;
-        double payment = loadAmount;
-        double monthlyInterestPayment = 0;
+        double payment = 0;
+        double interest = 0;
+        double remainingAmount = 0;
         double principalPaid = 0;
-        int months = 12;
+        int months = 1;
         StringBuilder sb = new StringBuilder();
-        if (monthlyPayment < 0) {
+        if (monthlyPayment <= 0) {
             return "Monthly payment is negative";
         }
-
+        payment = monthlyPayment;
         if (monthlyPayment > loadAmount) {
             return "Monthly payment is greater than the load amount";
         }
-        double paymentCheck = 0;
-
         sb.append(String.format("Month     Interest        Payment         Remaining%n"));
-        for (int i = 1; i < months; i++) {
-            // payment is static untill final month
 
-            // principalPaid is the monthly payment
-            if (principalPaid < 0) {
-                return "Monthly payment is greater than the load amount";
-            }
-
-            // calculate interest
-            monthlyInterestPayment = payment * monthlyInterestRate;
-            // calculate principal paid
-            principalPaid = monthlyPayment - monthlyInterestPayment;
-            // calculate remaining balance
-
-            // format and print
-            sb.append(
-                    String.format("%2d %15.2f %15.2f %15.2f%n", i + 1, monthlyInterestPayment, paymentCheck, payment));
-
+        while (loadAmount > 0) {
+            remainingAmount = Math.round((loadAmount - payment) * 100) / 100.0;
+            interest = Math.round(monthlyInterestRate * remainingAmount * 100) / 100.0;
+            payment = Math.round(monthlyPayment * 100) / 100.0;
+            sb.append(String.format("%2d %15.2f %15.2f %15.2f%n", months, interest, payment, remainingAmount));
+            months++;
         }
         return sb.toString();
     }
