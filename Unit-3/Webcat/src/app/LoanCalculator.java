30905f8f5557a725f355e234e39dea1cd857f230
Allusive <auraud.zarafshar@gmail.com>
2021-09-27 17:19:46 -0700

    webcat almost done

--- Unit-3/Webcat/src/app/LoanCalculator.java
+++ Unit-3/Webcat/src/app/LoanCalculator.java
@@ -1,7 +1,5 @@
 package app;
 
-import java.text.NumberFormat;
-
 public class LoanCalculator {
     public static void main(String[] args) {
         String table = createAmortizationTable(10000, 950, 0.17);
@@ -9,29 +7,41 @@ public class LoanCalculator {
     }
 
     public static String createAmortizationTable(double loadAmount, double monthlyPayment, double annualInterestRate) {
-        double monthlyInterestRate = annualInterestRate / 12;
-        double payment = 0;
-        double interest = 0;
-        double remainingAmount = 0;
-        double principalPaid = 0;
-        int months = 1;
-        StringBuilder sb = new StringBuilder();
-        if (monthlyPayment <= 0) {
-            return "Monthly payment is negative";
-        }
-        payment = monthlyPayment;
-        if (monthlyPayment > loadAmount) {
-            return "Monthly payment is greater than the load amount";
-        }
-        sb.append(String.format("Month     Interest        Payment         Remaining%n"));
 
-        while (loadAmount > 0) {
-            remainingAmount = Math.round((loadAmount - payment) * 100) / 100.0;
+        double monthlyInterestRate = annualInterestRate / 12;
+        double interest = 0;
+        double remainingAmount = loadAmount;
+        double temp = 0;
+        int months = 1;
+        // is not possible to pay off loan
+        if (monthlyPayment > loadAmount) {
+            return "Not possible to pay off the loan.";
+        }
+        String format = "";
+        StringBuilder sb = new StringBuilder();
+        format = format.concat(String.format("Month     Interest        Payment         Remaining%n"));
+        while (remainingAmount > 0) {
             interest = Math.round(monthlyInterestRate * remainingAmount * 100) / 100.0;
-            payment = Math.round(monthlyPayment * 100) / 100.0;
-            sb.append(String.format("%2d %15.2f %15.2f %15.2f%n", months, interest, payment, remainingAmount));
+            remainingAmount = Math.round((remainingAmount + interest) * 100) / 100.0;
+            temp = remainingAmount;
+            remainingAmount = remainingAmount - monthlyPayment;
+            if (remainingAmount <= 0) {
+                monthlyPayment = temp;
+            }
+            if (remainingAmount < 0) {
+                remainingAmount = 0;
+            }
+
+            format = format.concat(Integer.toString(months));
+            format = format.concat("         ");
+            format = format.concat(String.format("$%.2f", interest));
+            format = format.concat("\t  ");
+            format = format.concat(String.format("$%.2f", monthlyPayment));
+            format = format.concat("\t  ");
+            format = format.concat(String.format("$%.2f", remainingAmount));
+            format = format.concat("\n");
             months++;
         }
-        return sb.toString();
+        return format;
     }
 }
