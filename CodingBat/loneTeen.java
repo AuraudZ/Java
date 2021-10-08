19e2cc0b6db3ab411d749fffd9fa62e73e2f4a0c
Allusive <auraud.zarafshar@gmail.com>
2021-10-08 11:36:08 -0700

    Whole alot of codingbat

--- CodingBat/loneTeen.java
+++ CodingBat/loneTeen.java
@@ -1,3 +1,5 @@
+package CodingBat;
+
 public class loneTeen {
     public boolean loneTeen(int a, int b) {
         if ((a >= 13 && a <= 19) && (b >= 13 && b <= 19)) {
---
+++ Unit-3/MidpointCalculator.java
@@ -0,0 +1,25 @@
+
+public class MidpointCalculator {
+
+	public static void main(String[] args) {
+		int x1 = 3; int y1 = 5;
+		int x2 = -7; int y2 = 0;
+		
+		String x3 = "8"; String y3 = "10";
+		String x4 = "-10"; String y4 = "0";
+		
+		System.out.print("The midpoint between (" + x1  + ", " + y1 + ")");
+		System.out.print(" and (" + x2  + ", " + y2 + ") is: ");
+		// Calculate and print midpoint, given int inputs 
+		System.out.println("(" + Statistics.calculateAverage(x1, x2) + ", "
+				+ Statistics.calculateAverage(y1, y2) + ")");
+		
+		System.out.print("The midpoint between (" + x3  + ", " + y3 + ")");
+		System.out.print(" and (" + x4  + ", " + y4 + ") is: ");
+		// Calculate and print midpoint, given String inputs
+		System.out.println("(" + Statistics.calculateAverage(x3, x4) + ", "
+				+ Statistics.calculateAverage(y3, y4) + ")");
+
+	}
+
+}
