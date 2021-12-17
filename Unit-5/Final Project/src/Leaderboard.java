009875c68641fbe248afa56ef0373a7f26d2f317
Allusive <auraud.zarafshar@gmail.com>
2021-12-16 17:27:31 -0800

    Leaderboard dosen't work idc for now

--- Unit-5/Final Project/src/Leaderboard.java
+++ Unit-5/Final Project/src/Leaderboard.java
@@ -5,6 +5,49 @@ import javax.swing.*;
 public class Leaderboard extends JPanel {
     // JFrame Leaderboard that reads in the leaderboard.txt file and displays it
     // in a JTextArea
+    public String[] leaderBoardStrings() {
+        String[] leaderBoardString;
+        try {
+            File file = new File("leaderboard.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileReader fr = new FileReader(file.getAbsoluteFile());
+            BufferedReader br = new BufferedReader(fr);
+            leaderBoardString = br.lines().toArray(String[]::new);
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                leaderBoardString[i] = line;
+                i++;
+            }
+            br.close();
+            return leaderBoardString;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Paint the leaderboard
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        String[] strings = leaderBoardStrings();
+        int i = 0;
+        if (strings != null) {
+            for (String s : strings) {
+                String[] split = s.split(" ");
+                // Null check
+                if (split[0] != null || split[1] != null || split[2] != null || split[3] != null
+                        || split[4] != null || split[5] != null) {
+                    g.drawString(split[0] + " " + (Integer.parseInt(split[1])) + " " + split[2]
+                            + " " + split[3] + " " + split[4] + " " + split[5], 10, 10 + 20 * i);
+                    i++;
+                }
+            }
+        }
+    }
 
     public Leaderboard() {
         // JFrame
@@ -14,32 +57,8 @@ public class Leaderboard extends JPanel {
         frame.setLocation(100, 50);
         frame.setResizable(true);
         frame.setVisible(true);
-
-        // JTextArea
-        JTextArea textArea = new JTextArea();
-        textArea.setEditable(false);
-        textArea.setFont(new Font("Courier", Font.PLAIN, 12));
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        textArea.setBackground(Color.WHITE);
-        textArea.setForeground(Color.BLACK);
-        textArea.setText("Leaderboard:\n");
-
-        // Read in the leaderboard.txt file
-        try {
-            File file = new File("leaderboard.txt");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = br.readLine()) != null) {
-                textArea.append(line + "\n");
-            }
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Add the textArea to the frame
-        frame.add(textArea);
+        frame.setLayout(new BorderLayout());
+        frame.add(this, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
