15a1d213cb16fd778ee43c281f0e841a75d4fac5
Allusive <auraud.zarafshar@gmail.com>
2022-02-07 12:05:30 -0800

    7.5

--- Unit-7/7.5/SearchLibrary.java
+++ Unit-7/7.5/SearchLibrary.java
@@ -2,6 +2,7 @@
 public class SearchLibrary {
 
 	/*
+<<<<<<< HEAD
 	 * Search for 'num' in the given list using the Sequential Search algorithm. If Verbose Mode is
 	 * 'on', prints out the total number of checks This is to allow you to see the difference in the
 	 * number of checks when doing a Single Search Test. (Multiple Search Test would generate too
@@ -50,4 +51,40 @@ public class SearchLibrary {
 	}
 
 
+=======
+	 * Search for 'num' in the given list using the Sequential Search algorithm.
+	 * If Verbose Mode is 'on', prints out the total number of checks
+	 * This is to allow you to see the difference in the number of checks when
+	 * doing a Single Search Test. (Multiple Search Test would generate too much output).
+	 * Returns -1 if the number isn't found.
+	 * If found, return the index the number was found at
+	 */
+	public static int sequentialSearch(int num, int[] list, boolean verboseModeOn) {
+		int checkCount = 0;
+
+		if(verboseModeOn) 
+			System.out.println("Total number of checks: " + checkCount);
+		
+		return -1;
+	}
+	
+	/*
+	 * Search for 'num' in the given list using the Binary Search algorithm.
+	 * If Verbose Mode is 'on', prints out the total number of checks
+	 * This is to allow you to see the difference in the number of checks when
+	 * doing a Single Search Test. (Multiple Search Test would generate too much output).
+	 * Returns -1 if the number isn't found.
+	 * If found, return the index the number was found at
+	 */
+	public static int binarySearch(int num, int[] list, boolean verboseModeOn) {
+		int checkCount = 0;
+
+		if(verboseModeOn)
+			System.out.println("Total number of checks: " + checkCount);
+		
+		return -1;
+	}
+	
+	
+>>>>>>> e31b422 (7.5)
 }
