305d0b49dd7dd1ad4a9610290de87a532e293128
Allusive <auraud.zarafshar@gmail.com>
2022-02-14 11:50:19 -0800

    Done with 2D Arrays Tester

--- Unit-7/7.5/TwoDimensionalArrayTester.java
+++ Unit-7/7.5/TwoDimensionalArrayTester.java
@@ -3,7 +3,9 @@ public class TwoDimensionalArrayTester {
 		int[][] twoByThree = new int[2][3];
 		int[][] threeByThree = new int[3][3];
 		int[][] fourByFive = new int[4][5];
-		System.out.println(java.util.Arrays.toString(fourByFive));
+		for (int i = 0; i < fourByFive.length; i++) {
+			System.out.println(java.util.Arrays.toString(fourByFive[i]));
+		}
 
 		// Test countZeros method
 		System.out.println("Zeros: " + countZeros(fourByFive));
@@ -12,7 +14,9 @@ public class TwoDimensionalArrayTester {
 		fill1And3(fourByFive);
 
 		// Print out array, see if fill1And3() actually changed anything
-
+		for (int i = 0; i < fourByFive.length; i++) {
+			System.out.println(java.util.Arrays.toString(fourByFive[i]));
+		}
 
 		// Check if countZeros() properly detects the lack of 0's
 		System.out.println("Zeros: " + countZeros(fourByFive));
@@ -23,15 +27,28 @@ public class TwoDimensionalArrayTester {
 	// except where the row number is the same as the column number
 	// -- fill those with 3's
 	public static void fill1And3(int[][] vals) {
-
+		for (int i = 0; i < vals.length; i++) {
+			for (int j = 0; j < vals[i].length; j++) {
+				if (i == j) {
+					vals[i][j] = 3;
+				} else {
+					vals[i][j] = 1;
+				}
+			}
+		}
 	}
-
 
 	public static int countZeros(int[][] vals) {
-
-
-		return -1;
+		int count = 0;
+		// Plan: loop through each row (each row is an array)
+		// and go through each col.
+		for (int i = 0; i < vals.length; i++) {
+			for (int j = 0; j < vals[i].length; j++) {
+				if (vals[i][j] == 0) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
-
-
 }
