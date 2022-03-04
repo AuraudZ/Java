6bab52d72e8fa63cf5fb57680067d5a430aa5e64
Allusive <auraud.zarafshar@gmail.com>
2022-03-04 12:42:03 -0800

    This is the test code that was on paper lol

---
+++ Unit-8/8.5/src/SeatingChart.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeatingChart {
+
+    // Map student list collum by collum to a 2d array of students starting with index 0 of the list in 0,0 of the chart;
+    private Student[][] chart;
+
+    public SeatingChart(List<Student> studentList, int rows, int cols) {
+        chart = new Student[rows][cols];
+        int row = 0;
+        int col = 0;
+        for (Student student : studentList) {
+            chart[row][col] = student;
+            col++;
+            if (col == cols) {
+                row++;
+                col = 0;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("John", 0));
+        studentList.add(new Student("Jane", 1));
+        studentList.add(new Student("Jack", 2));
+        studentList.add(new Student("Jill", 3));
+
+        SeatingChart chart = new SeatingChart(studentList, 2, 2);
+        System.out.println(chart.chart[0][1].getName());
+        print2DArray(chart.chart);
+    }
+
+    public static void print2DArray(T[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ Unit-8/8.5/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    private String name;
+    private int absences;
+
+    public Student(String name, int absences) {
+        this.name = name;
+        this.absences = absences;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAbsences() {
+        return absences;
+    }
+}
