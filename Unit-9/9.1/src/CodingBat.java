4bbf9b2d104b9192a937c5bd21171b3922ebd250
Allusive <auraud.zarafshar@gmail.com>
2022-03-17 14:04:30 -0700

    new file:   src/SortLibraryForQuicksort.java

--- Unit-9/9.1/src/CodingBat.java
+++ Unit-9/9.1/src/CodingBat.java
@@ -12,7 +12,41 @@ public class CodingBat {
 
     public static void main(String[] args) {
         CodingBat cb = new CodingBat();
-        System.out.println(cb.parenBit("xyz(abc)123"));
+        System.out.println(cb.count11("11x111x1111"));
+        System.out.println(cb.strDist("cccatcowcatxx", "cat"));
     }
+
+    public int strCount(String str, String sub) {
+        if (str.length() < sub.length()) {
+            return 0;
+        }
+        if (str.substring(0, sub.length()).equals(sub)) {
+            return 1 + strCount(str.substring(sub.length()), sub); // Recursive count of substrings
+        }
+        return strCount(str.substring(1), sub);
+    }
+
+    public int count11(String str) {
+        if (str.length() < 2)
+            return 0;
+        if (str.substring(0, 2).equals("11")) {
+            return 1 + count11(str.substring(2));
+        }
+        return count11(str.substring(1));
+    }
+
+    // compute recursively the largest substring which starts and ends with sub and return its
+    // length.
+
+    public int strDist(String str, String sub) {
+        if (str.length() < sub.length()) {
+            return 0;
+        }
+        if (str.substring(0, sub.length()).equals(sub)) {
+            return sub.length() + strDist(str.substring(sub.length()), sub);
+        }
+        return strDist(str.substring(1), sub);
+    }
+
 
 }
--- Unit-9/9.1/src/SearchLibrary.java
+++ Unit-9/9.1/src/SearchLibrary.java
@@ -8,14 +8,14 @@ public class SearchLibrary {
 		}
 		return -1;
 	}
-	
-	
-	public static int binarySearch(int num, int[] list) {
+
+
+/*	public static int binarySearch(int num, int[] list) {
 		int low = 0;
 		int high = list.length - 1;
 		int medium = (low + high)/2;
 
-		while(low <= high) {  
+		while(low <= high) {
 			if(num == list[medium]) { // found!
 				return medium; // return index (not value)
 			}
@@ -27,10 +27,26 @@ public class SearchLibrary {
 				high = medium - 1;
 				medium = (low + high)/2;
 			}
-			
+
 		}
 		return -1; // not found!  return index of -1!
+	} */
+
+	public static int binarySearch(int num, int[] list) {
+		return binarySearchRecursive(num, list, 0, list.length/2, list.length - 1);
 	}
 
-	
+	public static int binarySearchRecursive(int num, int[] list, int low,int mid, int high) {
+
+		if (num == list[mid]) { // found!
+			return mid; // return index (not value)
+		}
+		if (low > high) { // not found!
+			return -1;
+		} else if (num > list[mid]) { // slice off left half of list
+			return binarySearchRecursive(num, list, mid + 1, (low + high) / 2, high);
+		} else { // cut off right half of list
+			return binarySearchRecursive(num, list, low, (low + high) / 2, mid - 1);
+		}
+	}
 }
