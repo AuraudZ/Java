312dcaf7280eaa1182ebe43d5ff288ac6b2301a1
Allusive <auraud.zarafshar@gmail.com>
2022-03-24 15:30:42 -0700

    Coding bat problems

--- Unit-9/9.2/src/Recursion2.java
+++ Unit-9/9.2/src/Recursion2.java
@@ -1,12 +1,10 @@
 public class Recursion2 {
     public static void main(String[] args) {
         Recursion2 cb = new Recursion2();
-        int[] nums = {2, 3};
+        int[] nums = {1, 2, 4, 8, 1};
         int start = 0;
-        int target = 17;
-//        System.out.println(cb.groupSum(start, nums, target));
-        System.out.println(cb.splitArray(nums));
-        System.out.println(equationSolver());
+        int target = 14;
+        System.out.println(cb.groupSumClump(start, nums, target));
     }
 
 
@@ -18,32 +16,81 @@ public class Recursion2 {
         // Recursive Case
         return groupSum(start + 1, nums, target - nums[start]) || groupSum(start + 1, nums, target);
     }
-
     /*
-    Given an array of ints, is it possible to divide the ints into two
-    groups, so that the sums of the two groups are the same.
-    Every int must be in one group or the other.
-    Write a recursive helper method that takes whatever arguments you like,
-    and make the initial call to your recursive helper from splitArray().
+    Given an array of ints, is it possible to choose a group of some of the ints,
+    beginning at the start index, such that the group sums to the given target?
+    However, with the additional constraint that all 6's must be chosen.
     (No loops needed.)
      */
-    public boolean splitArray(int[] nums) {
+    public boolean groupSum6(int start, int[] nums, int target) {
         // Base Case
-        if (nums.length == 0) {
-            return false;
+        if (start >= nums.length) {
+            return target == 0;
+        }
+        if (nums[start] == 6) {
+            return groupSum6(start + 1, nums, target - nums[start]);
+        }
+        return groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target);
+    }
+
+
+    public boolean groupSum5(int start, int[] nums, int target) {
+        // Base Case
+        if (start >= nums.length) {
+            return target == 0;
+        }
+
+        if(nums[start] % 5 == 0) {
+            if (start + 1 < nums.length && nums[start + 1] == 1) {
+                return groupSum5(start + 2, nums, target - nums[start]);
+            }
+            return groupSum5(start + 1, nums, target - nums[start]);
+        }
+
+        // If the value following a multiple of 5 is also a 1 then it is false
+        return groupSum5(start + 1, nums, target - nums[start]) || groupSum5(start + 1, nums, target);
+    }
+
+    public boolean groupNoAdj(int start, int[] nums, int target) {
+        // Base Case
+        if (start >= nums.length) {
+            return target == 0;
+        }
+        if (start + 2 < nums.length && nums[start] == nums[start + 2]) {
+            return groupNoAdj(start + 2, nums, target);
         }
         // Recursive Case
-        return splitArrayHelper(nums, 0, 0, nums[0]);
+        return groupNoAdj(start + 2, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target);
     }
 
-    private boolean splitArrayHelper(int[] nums, int start, int sum1, int sum2) {
+
+
+    //Given an array of ints, is it possible to choose a group of
+    // some of the ints, such that the group sums to the given target,
+    // with this additional constraint:
+    // if there are numbers in the array that are adjacent and the
+    // identical value, they must either all be chosen,
+    // or none of them chosen. For example,
+    // with the array {1, 2, 2, 2, 5, 2},
+    // either all three 2's in the middle must be chosen or not,
+    // all as a group.
+    // (one loop can be used to find the extent of the identical values).
+
+    public boolean groupSumClump(int start, int[] nums, int target) {
+        // Base Case
         if (start >= nums.length) {
-            return sum1 == sum2;
+            return target == 0;
         }
-        return splitArrayHelper(nums, start + 1, sum1, sum2) || splitArrayHelper(nums, start + 1, sum1 + nums[start], sum2);
+        int i = start;
+        while (i < nums.length && nums[i] == nums[start]) {
+            i++;
+        }
+        return groupSumClump(i, nums, target - nums[start]) || groupSumClump(i, nums, target);
     }
 
-  public static  int equationSolver() {
+
+
+    public static  int equationSolver() {
        double a = 3*Math.pow(-2,4);
        double b = -6*Math.pow(-2,3);
        double c = -5*-2;
