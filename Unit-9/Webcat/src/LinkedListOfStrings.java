bceaa1104edca7a542af342c0342a5543bc12618
Allusive <auraud.zarafshar@gmail.com>
2022-03-31 23:23:32 -0700

    Bahar Webcat

--- Unit-9/Webcat/src/LinkedListOfStrings.java
+++ Unit-9/Webcat/src/LinkedListOfStrings.java
@@ -7,23 +7,26 @@ public class LinkedListOfStrings {
     // iteratively traverse the Linked List, printing out the String + " --> "
     // unless it's the last node, in which case print out the String + " --> null"
     public String toString() {
-        String listString = "";
-        Node current = head;
-        while (current != null) {
-            listString += current.name + " --> ";
-            current = current.next;
+        String listString = "Head: ";
+        Node runner = head; // copy head reference to runner
+
+        while (runner != null) {
+            listString += runner.name + " --> ";
+            runner = runner.next;
         }
-        return listString + "null";
+        return listString + " null";
     }
 
     // iteratively traverse the Linked List, counting out the number of Nodes
     // whose String contains str
     public int countNodesWithString(String str) {
         int count = 0;
-        for (Node current = head; current != null; current = current.next) {
-            if (current.name.contains(str)) {
+        Node runner = head;
+        while (runner != null) {
+            if (runner.name.contains(str)) {
                 count++;
             }
+            runner = runner.next;
         }
         return count;
     }
@@ -31,36 +34,33 @@ public class LinkedListOfStrings {
     // recursively traverse the Linked List, counting out the number of Nodes
     // whose String contains str
     public int recursivelyCountNodesWithString(Node head, String str) {
-        // Base Case: if head is null reutrn
         if (head == null) {
             return 0;
         }
-        // Recursive Case: if head.name contains str, add 1 to count
         if (head.name.contains(str)) {
             return 1 + recursivelyCountNodesWithString(head.next, str);
+        } else {
+            return recursivelyCountNodesWithString(head.next, str);
         }
-        return recursivelyCountNodesWithString(head.next, str);
     }
 
     // Prints the nodes in reverse, iteratively
     public void printReversed(Node head) {
-        String reversedName = "";
-        Node current = head;
-        while (current != null) {
-            reversedName = current.name + "\n" + reversedName;
-            current = current.next;
+        String listString = "";
+        Node runner = head; // copy head reference to runner
+
+        while (runner != null) {
+            listString = runner.name + "\n" + listString;
+            runner = runner.next;
         }
-        reversedName += "null\n";
-        System.out.println(reversedName);
+        System.out.println(listString);
     }
 
     // Prints the nodes in reverse, recursively
     public void recursivelyPrintReversed(Node head) {
-        // Base Case: if head is null return
         if (head == null) {
             return;
         }
-        // Recursive Case: print the next node, then recursively print the next node
         recursivelyPrintReversed(head.next);
         System.out.println(head.name);
     }
@@ -75,49 +75,49 @@ public class LinkedListOfStrings {
     // (Returns false if this collection does not permit duplicates and already contains the specified element.)
     public boolean add(Node n) {
         Node runner = head;
-        if(head == null) {
+        if (head == null) {
             head = n;
+            nodeCount++;
             return true;
         }
-        while(runner != null) {
+        while (runner.next != null) {
             runner = runner.next;
         }
-        runner = n;
+        runner.next = n;
         nodeCount++;
         return true;
     }
 
-    public boolean add (String str) {
-        Node n = new Node(str);
-        return add(n);
-    }
-
     // Inserts the specified element at the specified position in this list.
     // Throws IndexOutOfBoundsException - if the index is out of range (index < 0 || index > size())
-    public void add(int index, Node n) throws IndexOutOfBoundsException {
-        Node current = head;
-        // If the index is invalid, throw an exception
-        if (index <= -1 || index > nodeCount) {
+    public void add(int index, Node n) {
+        // Cases to check for:
+        // 1. index == 0
+        // 2. index == size()
+        // 3. index > size()
+        Node runner = head;
+        if (index < 0 || index > size()) {
             throw new IndexOutOfBoundsException();
         }
-        // Make sure that if the index is 0 that we add the node to the front
         if (index == 0) {
             n.next = head;
             head = n;
+            nodeCount++;
+            return;
         }
-        // Iterate through the list until we get to the index
+
         for (int i = 0; i < index - 1; i++) {
-            current = current.next;
+            runner = runner.next;
         }
-        // Update the next node
-        if (index > 0) {
-            n.next = current.next;
-            current.next = n;
-        }
+
+        Node temp = runner.next;
+        runner.next = n;
+        n.next = temp;
         nodeCount++;
     }
 
-    // Removes all the elements from this list.
+
+    // Removes all of the elements from this list.
     public void clear() {
         while (head != null) {
             head = head.next;
@@ -127,16 +127,12 @@ public class LinkedListOfStrings {
 
     // Returns true if this list contains the specified element.
     public boolean contains(Node n) {
-        // Run through the list, checking each node for equality
-        for (Node current = head; current != null; current = current.next) {
-            if (current.equals(n)) {
+        Node runner = head;
+        for (int i = 0; i < size(); i++) {
+            if (runner.name.equals(n.name)) {
                 return true;
-            }
-            if (current.name.equals(n.name)) {
-                return true;
-            }
-            if (current.next == null) {
-                return false;
+            } else {
+                runner = runner.next;
             }
         }
         return false;
@@ -144,27 +140,28 @@ public class LinkedListOfStrings {
 
     // Returns the element at the specified position in this list
     // Throws IndexOutOfBoundsException - if the index is out of range (index < 0 || index >= size())
-    public Node get(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= nodeCount) {
+    public Node get(int index) {
+        if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException();
         }
-        Node current = head;
+        Node runner = head;
         for (int i = 0; i < index; i++) {
-            if (current == null) {
-                throw new IndexOutOfBoundsException();
-            }
-            current = current.next;
+            runner = runner.next;
+
         }
-        return current;
+        return runner;
+
     }
 
     // Removes the element at the specified position in this list.
     // Returns the element previously at the specified position
     // Throws IndexOutOfBoundsException - if the index is out of range (index < 0 || index >= size())
-    public Node remove(int index) throws IndexOutOfBoundsException {
+    public Node remove(int index) {
         if (index < 0 || index >= nodeCount) {
             throw new IndexOutOfBoundsException();
         }
+        // We need the rest of the list to be able to remove the node and we need to node we're removing to be able to return it
+        // So to do this we need to go to index - 1 and grab the node after the one we're removing and set the node after the one we're removing to null
         // If the index is 0, remove the head
         if (index == 0) {
             Node temp = head;
@@ -176,7 +173,6 @@ public class LinkedListOfStrings {
         Node current = head;
         for (int i = 0; i < index - 1; i++) {
             current = current.next;
-            // Once we get to the index, we need to save a reference to the node we want to remove to return it later
             if (i == index - 2) {
                 Node temp = current.next;
                 current.next = current.next.next;
@@ -187,27 +183,26 @@ public class LinkedListOfStrings {
         return null;
     }
 
+
     // Removes the first occurrence of the specified element from this list, if it is present.
     // Returns true if this list contained the specified element (or equivalently, if this list changed as a result of the call).
     public boolean remove(Node n) {
-        // If the first node is the node we want to remove, remove it
         if (head.name.equals(n.name)) {
             head = head.next;
             nodeCount--;
             return true;
         }
-
-        Node current = head;
-        Node list = head;
-        // Iterate through the list until we get to the node we want to remove
-        for (int i = 0; i < nodeCount; i++) {
-            if (current.name.equals(n.name)) {
-                list.next = current.next;
+        Node runner = head;
+        Node temp = head;
+        for (int i = 0; i < size(); i++) {
+            if (runner.name.equals(n.name)) {
+                temp.next = runner.next;
+                runner = runner.next;
                 nodeCount--;
                 return true;
             }
-            list = current;
-            current = current.next;
+            temp = runner;
+            runner = runner.next;
         }
         return false;
     }
@@ -215,7 +210,7 @@ public class LinkedListOfStrings {
 
     // Replaces the element at the specified position in this list with the specified element.
     // Throws IndexOutOfBoundsException - if the index is out of range (index < 0 || index >= size())
-    // Return the element previously at the specified position
+
     public Node set(int index, Node n) throws IndexOutOfBoundsException {
         Node current = head;
         if (index < 0 || index >= nodeCount) {
@@ -258,5 +253,6 @@ public class LinkedListOfStrings {
     public int size() {
         return nodeCount;
     }
-}
 
+
+}
---
+++ Unit-9/Webcat/src/Node.java
@@ -0,0 +1,8 @@
+public class Node {
+    String name;
+    Node next;
+
+    public Node(String name) {
+        this.name = name;
+    }
+}
